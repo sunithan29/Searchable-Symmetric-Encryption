59f8810bf2ad5b597b09fe9bdbc612523d2b8fdb
sunithan29 <sunithan29@gmail.com>
2019-12-22 22:26:53 -0500

    Update and rename sse_search.py to sse_query.py

--- sse_query.py
+++ sse_query.py
@@ -1,3 +1,15 @@
+#########
+#   
+#   sse_query.py
+#   
+#   Used to search the encrypted input file for 
+#   specific keywords that are saved as trapdoors.
+#   This prints the list of all identifiers
+#   for the keyword that is searched for.
+#
+#
+#########
+
 import pandas as pd
 from Crypto.Cipher import AES
 from Crypto.Hash import MD5
@@ -17,17 +29,19 @@ def search_index(document, trapdoor):
     result = [0]
     data_index = pd.read_csv(document)
     data_index = data_index.values
-    # start_time = time.time()
+   
     for row in range(data_index.shape[0]):
         if gen_codeword(row, trapdoor) in data_index[row]:
             result.append(row)
-
-    # print time.time() - start_time
+ 
     return result
 
 if __name__ == "__main__":
-
-  
+    
+    # Takes the input as the encrypted file 
+    # and the trapdoor file. List all the 
+    # identifiers that the keyword was found
+    
     index_file_name = input("\nPlease input the index file you want to search:  ")
     keyword_trapdoor_file = input("\nPlease input the file where trapdoor is stored:  ")
     result_keyword_trapdoor = open(keyword_trapdoor_file, "wb")
