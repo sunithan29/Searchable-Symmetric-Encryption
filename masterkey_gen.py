27192304c9af856bc70a42b49e39a38f452a49a9
sunithan29 <sunithan29@gmail.com>
2019-12-22 22:31:23 -0500

    Update masterkey_gen.py

--- masterkey_gen.py
+++ masterkey_gen.py
@@ -1,3 +1,12 @@
+##########
+#
+#  masterkey_gen.py
+#
+#  This script generates a masterkey that is used for
+#  the AES encryption scheme. Generate a new masterkey
+#  for every keyword search for stronger security.
+#
+##########
 import os
 import binascii
 
